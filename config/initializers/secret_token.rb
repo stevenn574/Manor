1fcc95070559ae51c360ad44e8eae4217a952448
Steve <steve@ubuntu.(none)>
2011-12-29 23:16:03 -0800

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Manor::Application.config.secret_token = '70540cc8e90dc1890c6324959e0bda25971103d2f92e820f60fba873cc15ce5cb46d2b9630eb93b50118bd58211ba5137572c49da83843038da89994cc8b1515'
