a6e73631086ab6fee73b93bd8028c6e4007750e5
Steve Naranjo <stevenn575@gmail.com>
2012-01-04 00:01:12 -0800

    update points

--- app/controllers/chores_controller.rb
+++ app/controllers/chores_controller.rb
@@ -20,16 +20,28 @@ class ChoresController < ApplicationController
       format.js
     end
   end
+  def update
+    @chore = Chore.find(params[:id])
+    @chore.update_attributes(params[:chore])
+    respond_to do |format|
+      format.js
+    end
+  end
   
   def destroy
     @chore = Chore.find(params[:id])
     @chore.destroy if @chore.user == current_user
-    redirect_to chores_url
+    respond_to do |format|
+      format.html do
+        redirect_to chores_url
+      end
+      format.js
+    end
   end
   
   private
   
   def todays_chores
-    Chore.includes(:chore_type).today.by_id 
+    Chore.includes(:chore_type).today.by_id
   end
 end
